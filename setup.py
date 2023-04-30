40cd6c2650bcfd1125b60df5aa191707bf39bdf3
Fasmina <fasmi1702@gmail.com>
2023-04-30 17:28:40 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project to build CNN model to Predict if the page is being flipped using a single image.',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
